f3ab0d1e1fe475a5c2f20e2de314ae7b06ffba8a
Abhi0pal <abhishekpal941163@gmail.com>
2025-08-18 13:00:01 +0530

    exploring  Get ,post,Commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
-
-app.use("/happy",(req,res)=>{
-    res.send("Hello this is HAppy Server");
-    
-});
-
-app.use("/test",(req, res) => {
-    res.status(404).send("Page not found!");
-});
+const app = express();
 
 
+// app.use("/happy", (req, res) => {
+//   res.send("Hello this is HAppy Server");
+// });
+// 
+// app.use("/test", (req, res) => {
+//   res.status(404).send("Page not found");
+// });
+// 
+// app.listen(3000, () => {
+//   console.log("Server is lostening on port 3000...");
+// });
+// 
+// app.use((req, res) => {
+//   res.send("This is the main page Now...");
+// });
 
+app.get("/user",(req,res)=>{
+    resizeBy.send({firstName:"abhi",LastName:"Pal"});
+})
+
+app.post("/user",(req,res)=>{
+    res.send("Data Sucessfully Saved to data base")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted succesfully");
+})
 
 app.listen(3000,()=>{
-    console.log("Server is lostening on port 3000...");
+    console.log("now data successfully loaded");
     
-});
+})
